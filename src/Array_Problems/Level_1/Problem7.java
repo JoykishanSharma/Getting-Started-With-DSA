f49614c0532d43cc9dd096a83f96512752415595
JoykishanSharma <joykishan120@gmail.com>
2021-06-14 14:07:28 +0530

    solved

---
+++ src/Array_Problems/Level_1/Problem7.java
@@ -0,0 +1,23 @@
+/*
+* Problem Statement : Sort an array of 0s, 1s and 2s
+*/
+
+package Array_Problems.Level_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Problem7 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int arraySize = scan.nextInt();
+        List<Integer> array = new ArrayList<>();
+        System.out.print("Enter array values : ");
+        for (int i = 0; i < arraySize; i++) array.add(scan.nextInt());
+        Collections.sort(array);
+        System.out.println(array);
+    }
+}
