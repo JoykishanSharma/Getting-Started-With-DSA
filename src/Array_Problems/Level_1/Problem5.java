1abd63e85f593bd52453493162d120dc6d6c3141
JoykishanSharma <joykishan120@gmail.com>
2021-06-14 13:47:25 +0530

    solved

---
+++ src/Array_Problems/Level_1/Problem5.java
@@ -0,0 +1,27 @@
+/*
+* Problem Statement : K’th Smallest/Largest Element in Unsorted Array
+*/
+
+package Array_Problems.Level_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int arraySize = scan.nextInt();
+        System.out.print("Enter array values : ");
+        List<Integer> array = new ArrayList<>();
+        for (int i = 0; i < arraySize; i++) array.add(scan.nextInt());
+        Collections.sort(array);
+        System.out.print("Enter k value : ");
+        int k = scan.nextInt();
+        System.out.println("Kth Smallest value is : " + array.get(k - 1));
+        Collections.reverse(array);
+        System.out.println("Kth Largest value is : " + array.get(k - 1));
+    }
+}
